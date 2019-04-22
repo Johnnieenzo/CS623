83630956e4118a436dd7f1f4498a0aa202b27ff7
Johnnieenzo <johnnieenzo@gmail.com>
2019-04-22 19:35:44 -0400

    Update Paper_Reviews.sql

--- Paper_Reviews.sql
+++ Paper_Reviews.sql
@@ -13,7 +13,7 @@ CREATE TABLE Author (
 );
 
 CREATE TABLE Paper (
-	Id 		 INT unsigned NOT NULL AUTO_INCREMENT,
+	Id       INT unsigned NOT NULL AUTO_INCREMENT,
 	Title	 VARCHAR(255) NOT NULL,
 	Abstract TEXT,
 	FileName VARCHAR(255) NOT NULL;
@@ -22,10 +22,10 @@ CREATE TABLE Paper (
 );
 
 CREATE TABLE Reviewer (
-	EmailAddr		 VARCHAR(255) NOT NULL,
-	FirstName		 VARCHAR(255) NOT NULL,
-	LastName		 VARCHAR(255) NOT NULL,
-	PhoneNum		 VARCHAR(255) NOT NULL,
+	EmailAddr	 VARCHAR(255) NOT NULL,
+	FirstName	 VARCHAR(255) NOT NULL,
+	LastName	 VARCHAR(255) NOT NULL,
+	PhoneNum	 VARCHAR(255) NOT NULL,
 	Affiliation	 VARCHAR(255) NOT NULL,
 	AuthorFeedback	 TEXT,
 	CommitteFeedback TEXT,
@@ -34,9 +34,9 @@ CREATE TABLE Reviewer (
 );
 
 CREATE TABLE Review (
-	Id 				 INT unsigned NOT NULL AUTO_INCREMENT,
-	PaperId			 INT unsigned NOT NULL,
-	ReviewId		 VARCHAR(255) NOT NULL,
+	Id 		 INT unsigned NOT NULL AUTO_INCREMENT,
+	PaperId		 INT unsigned NOT NULL,
+	ReviewId	 VARCHAR(255) NOT NULL,
 	Recommendation   TEXT NOT NULL,
 	MeritScore       INT NOT NULL,
 	ReadablityScore  INT NOT NULL,
@@ -49,7 +49,7 @@ CREATE TABLE Review (
 );
 
 CREATE TABLE Topic (
-	Id 		  INT unsigned NOT NULL AUTO_INCREMENT,
+	Id 	  INT unsigned NOT NULL AUTO_INCREMENT,
 	TopicName VARCHAR(255) NOT NULL
 
 );
@@ -63,7 +63,7 @@ CREATE TABLE Author_Sumbits_Paper (
 )；
 
 CREATE TABLE Paper_Assigned_Reviewer (
-	PaperId 	INT NOT NULL,
+	PaperId     INT NOT NULL,
 	ReviewerId  VARCHAR(255) NOT NULL,
 	FOREIGN KEY (PaperId) REFERENCES Paper (Id),
 	FOREIGN KEY (ReviewerId) REFERENCES Reviewer (EmailAddr)
